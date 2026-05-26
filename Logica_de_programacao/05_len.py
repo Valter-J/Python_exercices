a6db8dbac42715fba8d0a5caf661f58b2583e30d
Valter-J <valterjpereiras@gmail.com>
2026-05-26 16:49:47 -0300

    Atividades 01

---
+++ Logica_de_programacao/05_len.py
@@ -0,0 +1,28 @@
+# comando "len()"
+
+print(len("Quantas letras tem esse programa em py."))
+print(len("Hello World"))
+
+
+# Verificar se uma determinada frase ou caractere esté presente em uma String
+
+print("dia" in "Hoje é dia de python")
+
+# converter os caracteres para maiasculas
+
+print("Como assim o neymar vai pra copa".upper())
+
+
+# converter os caracteres para minusculas
+
+print("OLa BOm Dia.".lower())
+
+# remover espaços em brancos das "Pontas" . "Strip()"
+
+print("   testando    os  espaços   ".strip())
+
+
+# Substitir uma string por outra: replace()
+
+
+print("Cebolinha só fala eLado".replace("L", "rr"))
