4e71ca8e516f486ab2acc6fe3556720484bb3281
Valter-J <valterjpereiras@gmail.com>
2026-06-25 21:39:28 -0300

    Exercicio 07, atividade 03

--- Exercicios_python/Atividade_03/Atividade_1.1.py
+++ Exercicios_python/Atividade_03/Atividade_1.1.py
@@ -1,16 +1,11 @@
+from random import randint
+
 #  Exercício 07
 # Dado o código Python abaixo, você deve estende-lo para adicionar cada 
 # uma das funcionalidades descritas abaixo:
 
 
 # Crie uma lista com 100 elementos contendo números inteiros aleatórios entre 1 e N
-# Calcule e imprima qual é o maior e o menor elemento desta lista
-# Copie a lista que você gerou para outra lista, mas contendo apenas os números maiores do que N/2
-# Limpe a lista original
-# Selecione um item de uma posição aleatória da lista e o remova
-# Calcule e imprima qual é o segundo maior e o segundo menor elemento desta lista
-
-from random import randint
 
 lista = []
 
@@ -20,4 +15,14 @@ for _ in range(n):
     lista.append(randint(1,100))
 
 print(lista)
+
+# Calcule e imprima qual é o maior e o menor elemento desta lista
+
+
+
+# Copie a lista que você gerou para outra lista, mas contendo apenas os números maiores do que N/2
+# Limpe a lista original
+# Selecione um item de uma posição aleatória da lista e o remova
+# Calcule e imprima qual é o segundo maior e o segundo menor elemento desta lista
+
 
