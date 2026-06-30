418639546f56b8a2bd862597cbcca0afaa0905de
Valter-J <valterjpereiras@gmail.com>
2026-06-29 22:14:59 -0300

    Papel 4

--- Exercicios_python/Atividade_04/Atividade_01.py
+++ Exercicios_python/Atividade_04/Atividade_01.py
@@ -38,27 +38,4 @@ x= [x*x for x in range(13)]
 for imprimi in x:
     print(imprimi)
 
-# Papel 4: O Detentor Mais Procurado
-# Programa exemplo "Escreva um programa que leia um 10 "
-# "números inteiros e imprima qual é o maior e o menor dos"
-# " números lidos"
-
-# Leitura do primeiro número
-numero = int(input("Digite o 1º número: "))
-maior = numero
-menor = numero
-
-# Leitura dos outros 9 números
-for i in range(2, 11):
-    numero = int(input(f"Digite o {i}º número: "))
-
-    if numero > maior:
-        maior = numero
-
-    if numero < menor:
-        menor = numero
-
-# Exibição dos resultados
-print("Maior número:", maior)
-print("Menor número:", menor)
 
---
+++ Exercicios_python/Atividade_04/Atividade_1.1.py
@@ -0,0 +1,24 @@
+# Papel 4: O Detentor Mais Procurado
+# Programa exemplo "Escreva um programa que leia um 10 "
+# "números inteiros e imprima qual é o maior e o menor dos"
+# " números lidos"
+
+# Leitura do primeiro número
+numero = int(input("Digite o 1º número: "))
+maior = numero
+menor = numero
+
+# Leitura dos outros 9 números
+for i in range(2, 11):
+    numero = int(input(f"Digite o {i}º número: "))
+
+    if numero > maior:
+        maior = numero
+
+    if numero < menor:
+        menor = numero
+
+# Exibição dos resultados
+print("Maior número:", maior)
+print("Menor número:", menor)
+
