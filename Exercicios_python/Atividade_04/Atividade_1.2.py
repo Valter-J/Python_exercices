468c94a0ad4813670f1766e24877f91897be7ed6
Valter-J <valterjpereiras@gmail.com>
2026-06-30 00:09:05 -0300

    Papel 06, Atividade 04

---
+++ Exercicios_python/Atividade_04/Atividade_1.2.py
@@ -0,0 +1,19 @@
+# Programa exemplo
+# Escreva um programa que leia 10 números inteiros, os armazene em uma lista e posteriormente 
+# calcule a soma de todos os elementos da lista.
+
+
+numero = []
+
+for i in range(1, 11):
+  numero.append(int(input("Digite um numero de 0 a 100: ")))
+  soma = 0
+
+  for a in numero:
+    soma = soma + a
+
+
+
+print("------------resultado-----------")
+print(numero)
+print(soma)
