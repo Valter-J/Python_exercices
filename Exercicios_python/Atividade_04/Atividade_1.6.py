45a7f5762383efc9e8cdeeb402ad1268ad7e1bd8
Valter-J <valterjpereiras@gmail.com>
2026-07-14 23:18:34 -0300

    Papel 9, finalmente entendi

--- Exercicios_python/Atividade_04/Atividade_1.6.py
+++ Exercicios_python/Atividade_04/Atividade_1.6.py
@@ -7,7 +7,7 @@ from random import randint
 
 lista=[]
 
-for _ in range (10):
+for _ in range(1,10):
     lista.append(randint(1, 50))
 
 print(lista)
@@ -15,13 +15,21 @@ print(lista)
 lista2=[]
 
 for i in lista:
+    
+    inserir = False 
+    
+    for a in range(len(lista2)):
+        valor = lista2[a]
+        if ( i < valor):
+            lista2.insert(a, i)
+            inserir = True
+            break
+        
+    if inserir == False:
+        lista2.append(i)
+            
+        
 
-     for a in lista:
-        if ( i >= i):
-            lista2.append(i)
-        else:
-            contador= len(lista2)
-            lista2.insert(contador - 1, i)
 
 
 
